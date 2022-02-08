b11c625e9f7cdcc6382e6a742c92b7c204d72bcb
Frederik Heymann <frederik.heymann@gmx.de>
2022-02-08 16:53:32 +0100

    Programm erweitert

--- asteroids.py
+++ asteroids.py
@@ -1,54 +1,82 @@
 from typing import Set
 import pygame
 import os
-from random import randint 
+from math import *
 
-#Ich besitze keinerlei Rechte an den, in diesem Programm, verwendeten Bildern.
-#Mit den diesem Programm wird kein kommerzieller Gewinn erzielt.
-#Die in diesem Programm verwendeten Bilder stammen von den vollgenden Internetseiten:
-#'djungle_rain.png' stammt von https://www.umdiewelt.de/t4455_18 (www.umdiewelt.de)
-#'dragonfly.png' stammt von https://miausmiles.com/2011/incredible-random-stuff/draw-something-every-day-035 (www.miausmiles.com)
-#'drop.png' stammt von http://www.clipartpanda.com/clipart_images/domain-raindrop-clip-art-19285059 (www.clipartpanda.com)
-#Ich bedanke mich bei den Contan Creatorn für ihre gute Arbeit
 
 class Settings(object):
-    window_height = 780
-    window_width = 600
+    window_height = 800
+    window_width = 500
     path_file = os.path.dirname(os.path.abspath(__file__))
     path_image = os.path.join(path_file, "images")
-    dragonfly_size = (50, 60) 
-    title = "Liebelle im Regen"
+    title = "Raumschiff"
 
-class Dragonfly(pygame.sprite.Sprite):
+class Timer(object):
+    def __init__(self, duration, with_start = True):
+        self.duration = duration
+        if with_start:
+            self.next = pygame.time.get_ticks()
+        else:
+            self.next = pygame.time.get_ticks() + self.duration
+
+    def is_next_stop_reached(self):
+        if pygame.time.get_ticks() > self.next:
+            self.next = pygame.time.get_ticks() + self.duration
+            return True
+        return False
+
+    def change_duration(self, delta=10):
+        self.duration += delta
+        if self.duration < 0:
+            self.duration = 0
+
+class Space_Ship(pygame.sprite.Sprite):
     def __init__(self, filename) -> None:
         super().__init__()
         self.image_orig = pygame.image.load(os.path.join(Settings.path_image, filename)).convert_alpha()
-        self.image = pygame.transform.scale(self.image_orig, Settings.dragonfly_size)
+        self.image = self.image_orig
         self.rect = self.image.get_rect()
         self.mask = pygame.mask.from_surface(self.image)       
-        self.rect.centerx = Settings.window_width / 2          #Spielersprite wird plaziert in der Mitten des Bildschirms
-        self.rect.bottom = Settings.window_height -10
-        self.speed = 5
+        self.rect.centerx = Settings.window_width / 2
+        self.rect.centery = Settings.window_height / 2
+        self.rotation = 0
+        self.speed_x = 0
+        self.speed_y = 0
     #Bewegen des Spielers
-    def watch_for_move(self):
-        press = pygame.key.get_pressed()                       #registiert Tastendruck
-        if press[pygame.K_UP]:
-            self.rect.top -= self.speed
-        if press[pygame.K_DOWN]:
-            self.rect.top += self.speed
-        if press[pygame.K_RIGHT]:
-            self.rect.left += self.speed
-        if press[pygame.K_LEFT]:
-            self.rect.left -= self.speed
-        #Wandkollision
-        if self.rect.top <= 5:
-            self.rect.top += 5
-        if self.rect.bottom >= Settings.window_height -5:
-            self.rect.top -= 5
-        if self.rect.left <= 5:
-            self.rect.left += 5                                
-        if self.rect.right >= Settings.window_width -5:
-            self.rect.right -= 5
+    def update(self):
+
+        self.rect.move_ip((self.speed_x, self.speed_y))
+
+        if self.rect.top >= Settings.window_height:
+            self.rect.bottom = 1
+        if self.rect.bottom <= 0:
+            self.rect.top = Settings.window_height
+        if self.rect.left >= Settings.window_width:
+            self.rect.right = 1
+        if self.rect.right <= 0:
+            self.rect.left = Settings.window_width
+
+    def move(self):
+        self.speed_x = round(self.speed_x - sin(self.rotation), 0)
+        self.speed_y = round(self.speed_y - cos(self.rotation), 0)
+    
+    def rotate_right(self):
+        if self.rotation == 0:
+            self.rotation = 360
+        self.rotation -= 22.5
+        c = self.rect.center
+        self.image = pygame.transform.rotate(self.image_orig, self.rotation)
+        self.rect = self.image.get_rect()
+        self.rect.center = c
+    
+    def rotate_left(self):
+        if self.rotation == 360:
+            self.rotation = 0
+        self.rotation += 22.5
+        c = self.rect.center
+        self.image = pygame.transform.rotate(self.image_orig, self.rotation)
+        self.rect = self.image.get_rect()
+        self.rect.center = c
         
 
     def draw(self, screen):
@@ -62,12 +90,12 @@ class Game(object):
         self.screen = pygame.display.set_mode((Settings.window_width, Settings.window_height))
         pygame.display.set_caption(Settings.title)
         self.clock = pygame.time.Clock()
-        self.dragonfly_group = pygame.sprite.Group()
+        self.space_ships_group = pygame.sprite.Group()
+        self.space_ship = Space_Ship('ship0.png')
+        self.space_ships_group.add(self.space_ship)
         self.running = True
         self.counter = 0
-        self.dragonflys = 0
         self.lives = 3
-        self.screen = pygame.display.set_mode((Settings.window_width, Settings.window_height))
 
     def run(self):
         while self.running:
@@ -83,29 +111,22 @@ class Game(object):
             if event.type == pygame.KEYDOWN:        
                 if event.key == pygame.K_ESCAPE:   
                     self.running = False
-                if event.key == pygame.K_r:         
-                    self.dragonfly_image = pygame.transform.rotate(self.dragonfly.image, 90)
+                if event.key == pygame.K_RIGHT:         
+                    self.space_ship.rotate_right()
+                if event.key == pygame.K_LEFT:         
+                    self.space_ship.rotate_left()
+                if event.key == pygame.K_UP:         
+                    self.space_ship.move()
             elif event.type == pygame.QUIT:         
-                self.running = False
+                self.running = False      
 
     def player(self):
-        if self.dragonflys == 0 and self.lives > 0:           #falls der Spieler kollidiert und er noch Leben übrig hat
-            self.dragonfly = Dragonfly('ship0.png')
-            self.dragonfly_group.add(self.dragonfly)          #wird ein neuer Spieler erstellt
-            self.dragonflys = 1                               #self.dragonflys ist 1 wenn der Spieler noch nicht collidiert ist
-        else:
-            pass
-        self.dragonfly.watch_for_move()
+        self.space_ship.update()
 
-
-        if self.dragonflys == 0: 
-            self.drop_group.empty()      #wenn ein Spieler neu erscheinen muss werden werden alle Hindernisse entfernt
-            #aktualisieren Hindernisspunktestand
-            self.life_counter = self.font_counter.render('Remaining Lives: ' + str(self.lives), True, [255, 0, 0])
         
     def draw(self):
         self.screen.fill((0, 0, 0))
-        self.dragonfly.draw(self.screen)
+        self.space_ship.draw(self.screen)
         pygame.display.flip()
 
 
